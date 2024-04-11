e4b2201120eea556d0e717ac1a0f74d0225920ad
Dmitriyz26 <166714195+Dmitriyz26@users.noreply.github.com>
2024-04-11 14:44:39 +0300

    Create scriptjava.js

---
+++ scriptjava.js
@@ -0,0 +1,5 @@
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission
+    // You can add code here to handle form submission, e.g., sending data to server via AJAX
+    alert('Спасибо! Ваше сообщение отправлено.');
+});
